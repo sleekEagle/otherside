5ae18356186c64e47be59bc7310864472f58d95b
sleekEagle <lahirunuwanisme@gmail.com>
2025-11-17 14:55:20 -0600

    summerize with gemini API

---
+++ summerize_geminiAPI.py
@@ -0,0 +1,35 @@
+from google import genai
+from helper import read_yaml
+from transcribe import YouTubeTranscriber
+
+# ytt = YouTubeTranscriber()
+# tr = ytt.get_transcript("https://www.youtube.com/watch?v=nuIgsUM9GgM")
+# tr.content
+# with open("transcript.txt", "w", encoding="utf-8") as f:
+#     f.write(tr.content)
+
+class SummerizeGeminiAPI:
+    def __init__(self):
+        CRED_PATH = './credentials.yaml'   
+        cred = read_yaml(CRED_PATH)
+        API_KEY = cred.get("gemini")
+        self.client = genai.Client(api_key=API_KEY)
+
+    def summerize(self, transcript: str) -> str:
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash", contents=f'summerize the following youtube transcript as bullet points and nothing else: {transcript}'
+        )
+        return response.text
+    
+
+'''
+How to use SummerizeGeminiAPI:
+# get transcript first
+ytt = YouTubeTranscriber()
+tr = ytt.get_transcript("https://www.youtube.com/watch?v=nuIgsUM9GgM")
+
+# summerize
+sg = SummerizeGeminiAPI()
+summary = sg.summerize(tr.content)
+
+'''
--- transcribe.py
+++ transcribe.py
@@ -4,30 +4,6 @@ https://github.com/jdepoix/youtube-transcript-api
 from youtube_transcript_api import YouTubeTranscriptApi
 from supadata import Supadata, SupadataError
 from helper import read_yaml
-CRED_PATH = './credentials.yaml'
-cred = read_yaml(CRED_PATH)
-SUPADATA_KEY = cred.get("supadata")
-assert SUPADATA_KEY , "You must set your Supadata API key in credentials.yaml"
-
-supadata = Supadata(api_key=SUPADATA_KEY)
-'''
-how to use get_transcript_supadata:
-tr = get_transcript_supadata("https://www.youtube.com/watch?v=nuIgsUM9GgM")
-get string of the transcript: 
-tr.content
-'''
-def get_transcript_supadata(video_url):
-    try:
-        transcript = supadata.transcript(
-            url=video_url,
-            lang="en",  # Optional: preferred language
-            text=True,  # Optional: return plain text instead of timestamped chunks
-            mode="auto"  # Optional: "native", "auto", or "generate"
-        )
-        return transcript
-    except SupadataError as e:
-        print(f"Error fetching transcript: {e}")
-        return None
 
 
 # video_id = "yKuwrnCfsFQ"  # The part after "v=" in YouTube URL
@@ -35,10 +11,41 @@ def get_transcript_supadata(video_url):
 # for snippet in tr:
 #     print(snippet.text)
 
-def get_trancript_ytTapi(video_id):
-    ytt_api = YouTubeTranscriptApi()
-    tr = ytt_api.fetch(video_id)
-    return tr
+# def get_trancript_ytTapi(video_id):
+#     ytt_api = YouTubeTranscriptApi()
+#     tr = ytt_api.fetch(video_id)
+#     return tr
+
+
+class YouTubeTranscriber:
+    def __init__(self):
+        CRED_PATH = './credentials.yaml'
+        cred = read_yaml(CRED_PATH)
+        SUPADATA_KEY = cred.get("supadata")
+        assert SUPADATA_KEY , "You must set your Supadata API key in credentials.yaml"
+        self.supadata = Supadata(api_key=SUPADATA_KEY)
+    
+    '''
+    how to use get_transcript_supadata:
+    tr = get_transcript_supadata("https://www.youtube.com/watch?v=nuIgsUM9GgM")
+    get string of the transcript: 
+    tr.content
+    '''
+    def get_transcript(self, video_url: str):
+        try:
+            transcript = self.supadata.transcript(
+                url=video_url,
+                lang="en",  # Optional: preferred language
+                text=True,  # Optional: return plain text instead of timestamped chunks
+                mode="auto"  # Optional: "native", "auto", or "generate"
+            )
+            return transcript
+        except SupadataError as e:
+            print(f"Error fetching transcript: {e}")
+            return None
+
+
+
 
 
 
