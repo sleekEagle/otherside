254615b825a7ee33a24f51f9f52674f13569e47c
sleekEagle <lahirunuwanisme@gmail.com>
2025-12-15 09:59:36 +0530

    concat summaries

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any
 import yaml
+import os
 
 def read_yaml(path: str | Path) -> Any:
     """
@@ -12,3 +13,25 @@ def read_yaml(path: str | Path) -> Any:
         raise FileNotFoundError(f"YAML file not found: {p}")
     with p.open("r", encoding="utf-8") as f:
         return yaml.safe_load(f)
+    
+
+def concat_summaries(dir):
+    files = [file for file in os.listdir(dir) if file.endswith('.txt')]
+    files.sort()
+    concat = ''
+    for i,file in enumerate(files):
+        if file == 'all_summaries.txt':
+            continue
+        concat += f'\n\n=== Video {i+1} ===\n\n'
+        file_path = os.path.join(dir, file)
+        with open(file_path, 'r') as f:
+            content = f.read()
+            concat += content
+    out_path = os.path.join(dir, 'all_summaries.txt')
+    with open(out_path, 'w', encoding='utf-8') as f:
+        f.write(concat)
+    pass
+
+
+# concat_summaries("summary")
+
